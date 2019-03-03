f1ae33c61ddb63e1d55b452e7745bb1ddd602883
ups311997 <upasanapoddar3@gmail.com>
2019-03-03 16:17:58 +0530

    fourth

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,8 +1,10 @@
 package com.stackroute;
 
 import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 
 public class Main {
@@ -14,10 +16,15 @@ public class Main {
 
 
 
-//        ApplicationContext applicationContext1 = new ClassPathXmlApplicationContext("JavaBean.xml");
-//        Movie movieFirst= (Movie)applicationContext1.getBean("movie2");
-//
-//        System.out.println(movieFirst.getActor());
+        XmlBeanFactory xmlBeanFactory= new XmlBeanFactory ( new ClassPathResource("JavaBean.xml"));
+
+        Movie movieFirst= (Movie)xmlBeanFactory.getBean("movie2");
+
+        System.out.println(movieFirst.getActor());
+
+        movie.setApplicationContext(applicationContext);
+        movieFirst.setBeanFactory(xmlBeanFactory);
+
 
     }
 }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,7 +1,17 @@
 package com.stackroute.domain;
 
-public class Movie {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie implements ApplicationContextAware, BeanNameAware , BeanFactoryAware {
     Actor actor;
+    private ApplicationContext applicationContext=null;
+
+
 
     @Override
     public String toString() {
@@ -23,4 +33,16 @@ public class Movie {
     }
 
 
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+        System.out.println(applicationContext);
+    }
+
+    public void setBeanName(String beanName) {
+        System.out.println("Bean Name is "+beanName);
+    }
+
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println(beanFactory);
+    }
 }
